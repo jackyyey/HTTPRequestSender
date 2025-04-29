59f50248b3bd71f1b794ce0db4566253483c2a5a
Jacky Ye <6272880@cegepmontpetit.ca>
2025-04-28 21:05:54 -0400

    fct httpsender

--- HTTPSender/Program.cs
+++ HTTPSender/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -24,9 +25,11 @@ namespace HTTPSender
             string domaine = Console.ReadLine();
             Console.WriteLine("Enter Task Id");
             string id = Console.ReadLine();
-            if (true) //Regex.Match(domaine, "[A-Za-z0-9]+\\.[A-Za-z0-9]+").Success
+            Console.WriteLine("Is domain name a standard domain name? (y/n)");
+            if (Console.ReadLine().ToLower() == "y" ? Regex.Match(domaine, "[A-Za-z0-9]+\\.[A-Za-z0-9]+").Success : true) //Regex.Match(domaine, "[A-Za-z0-9]+\\.[A-Za-z0-9]+").Success
             {
-                HttpClient httpClient = new HttpClient() { BaseAddress = new Uri("http://"+domaine+"/") };
+                HttpClientHandler httpClientHander = new HttpClientHandler();
+                HttpClient httpClient = new HttpClient(httpClientHander) { BaseAddress = new Uri("http://"+domaine+"/") };
                 using StringContent queryBody = new(
                     JsonSerializer.Serialize(new
                     {
@@ -40,15 +43,28 @@ namespace HTTPSender
                 using HttpResponseMessage response = await httpClient.PostAsync("api/id/signup", queryBody);
                 if (response.IsSuccessStatusCode)
                 {
-                    IEnumerable<string> cookies = response.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
+                    Console.WriteLine("Insert new task value:");
+                    try
+                    {
+                        int progress = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Value not a number, retrying in 5 seconds");
+                        Thread.Sleep(5000);
+                        Console.Clear();
+                        await start();
+                    }
+                    var cookies = httpClientHander.CookieContainer.GetCookies(new Uri("http://" + domaine + "/"));
                     var cookieContainer = new CookieContainer();
                     using (var handler = new HttpClientHandler() { CookieContainer = cookieContainer })
                     using (var client = new HttpClient(handler) { BaseAddress = httpClient.BaseAddress })
                     {
                         var cookie = cookies.First();
-                        cookieContainer.Add(httpClient.BaseAddress, new Cookie("JSESSIONID", cookies.First()));
-                        var result = await client.GetAsync("/api/progres/" + id + "/100");
-                        var help = new List<int>();
+                        cookieContainer.Add(httpClient.BaseAddress, new Cookie("JSESSIONID", cookies.Where(s=>s.Name == "JSESSIONID").First().Value));
+                        var result = await client.GetAsync("/api/progress/" + id + "/100");
+                        Console.WriteLine("Success");
+                        System.Environment.Exit(0);
                     }
                 }
                 else
